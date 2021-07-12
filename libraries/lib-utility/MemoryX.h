b4790d3e7c15ee8bebb825389250c4b34303a4de
meguminloli <83077245+meguminloli@users.noreply.github.com>
2021-07-12 17:36:17 +0300

    managed to build without conan

--- libraries/lib-utility/MemoryX.h
+++ libraries/lib-utility/MemoryX.h
@@ -586,16 +586,7 @@ OutContainer transform_container( InContainer &inContainer, Function &&fn )
  If a structure contains any members with large alignment, this base class may also allow it to work in
  macOS builds under current limitations of the C++17 standard implementation.
  */
-struct UTILITY_API alignas(
-#ifdef __WIN32__
-   std::hardware_destructive_interference_size
-#else
-   // That constant isn't defined for the other builds yet
-   64 /* ? */
-#endif
-)
-
-NonInterferingBase {
+struct alignas(64) NonInterferingBase {
    static void *operator new(std::size_t count, std::align_val_t al);
    static void operator delete(void *ptr, std::align_val_t al);
 
